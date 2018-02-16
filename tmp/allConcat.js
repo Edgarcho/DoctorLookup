78478b4b50d6bfe42befcec0dbba784c52f49f78
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-16 11:21:56 -0800

    Install gulp-concat package

---
+++ tmp/allConcat.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('#lookup-form').submit(function(event){
+    event.preventDefault();
+    var medicalIssue = $('#medical').val();
+    $('#result')
+  });
+});
+
+$(document).ready(function(){
+  $('#lookup-by-name-form').submit(function(event){
+    event.preventDefault();
+    var doctorName = $('#name').val();
+    $('#result')
+  });
+});
