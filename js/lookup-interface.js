4eadca0898031a652a036fdab68667a6ef63af3c
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-16 15:35:26 -0800

    Add api calls in lookup-interface

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -3,7 +3,21 @@ var apiKey = require('./../.env').apiKey;
 $(document).ready(function(){
   $('#lookup-form').submit(function(event){
     event.preventDefault();
-    var medicalIssue = $('#medical').val();
-  //  $('#result')
+    const location = (45.523062, -122.676482, 100);
+    let medicalIssue = $('#medical').val();
+    $('#medical').val("");
+    $.ajax({
+      url: `http://https://api.betterdoctor.com/2016-03-01/doctors?query=medicalIssue&location=location&user_key=apiKey`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('#result').append('Here are the result for ${medicalIssue} is ${response.data}%');
+      },
+      error: function(){
+        $('#errors').text("There was an errir processing your request. Please try again.")
+      }
+    });
   });
 });
