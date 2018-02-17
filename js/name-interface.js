55c5c1e2faf817f7c77c4821bb7c7f73a5a95730
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-16 16:10:41 -0800

    Delete name-lookup.js

--- js/name-interface.js
+++
@@ -1,9 +0,0 @@
-var apiKey = require('./../.env').apiKey;
-
-$(document).ready(function(){
-  $('#lookup-by-name-form').submit(function(event){
-    event.preventDefault();
-    var doctorName = $('#name').val();
-  //  $('#result')
-  });
-});
